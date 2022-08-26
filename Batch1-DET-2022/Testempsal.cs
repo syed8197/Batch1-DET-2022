2d06587be59fe29c72a1ff131c112d813452b312
syed8197 <tsyed5364@gmail.com>
2022-08-26 18:20:16 +0530

    change

---
+++ Batch1-DET-2022/Testempsal.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Batch1_DET_2022
+{
+    internal class Testempsal
+    {
+        public static void Main()
+        {
+            empsal e = new empsal(22855, "monika", 584123, "banglore", 914866767, 30000, 1000);
+            Console.WriteLine($"emp netsal is{e.GetNetSal()}");
+            Console.WriteLine($"emp grade is {e.GetEmpGrade()}");
+        }
+    }
+}
---
+++ Batch1-DET-2022/empsal.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Batch1_DET_2022
+{
+    internal class empsal
+    {
+        int Id;
+        string name;
+        int pincode;
+        string address;
+        int phno;
+        float grosssal;
+        float pf;
+
+        public empsal(int Id, string name, int pincode, string address, int phno, float grosssal, float pf)
+        {
+            this.Id = Id;
+            this.name = name;
+            this.pincode = pincode;
+            this.address = address;
+            this.phno = phno;
+            this.grosssal = grosssal;
+            this.pf = pf;
+
+        }
+        public float GetNetSal()
+        {
+
+            return (grosssal - pf);
+        }
+        public char GetEmpGrade()
+        {
+            if (GetEmpGrade() > 10000)
+            {
+                return 'A';
+            }
+            else if (GetEmpGrade() > 5000)
+            {
+                return 'B';
+            }
+            else
+                return 'C';
+        }
+    }
+}
+
